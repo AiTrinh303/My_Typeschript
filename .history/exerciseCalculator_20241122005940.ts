86dacfbc453360eeb1891270551819ad69016547
AiTrinh303 <trong.trinh@dci-student.org>
2024-11-22 09:37:50 +0100

    done with exercise 9.2 9.3

---
+++ .history/exerciseCalculator_20241122005940.ts
@@ -0,0 +1,50 @@
+// 9.2 Exercise calculator
+import { calculateBmi } from "./bmiCalculator"
+
+interface ExcerciseResult {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = ( target: number, dailyHours: number[]): ExcerciseResult => {
+    const periodLength = dailyHours.length;
+    const trainingDays = dailyHours.filter(hours => hours > 0).length;
+    let success = false;
+    if (trainingDays >= periodLength) {
+        success = true;
+    }
+    const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength;
+
+    let rating : number;
+    let ratingDescription : string;
+
+    if (average < target) {
+        rating = 1;
+        ratingDescription = 'not too bad but could be better';
+    } else if (average === target) {
+        rating = 2;
+        ratingDescription = 'good but you need to put in more effort!';
+    } else {
+        rating = 3;
+        ratingDescription = 'You are doing great!';
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+   }
+
+// console.log(calculateExercises(2, [1, 0, 2, 4.5, 0, 3, 1, 0, 4]))   
+
+console.log
---
+++ .history/exerciseCalculator_20241122093554.ts
@@ -0,0 +1,53 @@
+// 9.2 Exercise calculator
+import { calculateBmi } from "./bmiCalculator"
+
+interface ExcerciseResult {
+    periodLength: number,
+    trainingDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+const calculateExercises = ( target: number, dailyHours: number[]): ExcerciseResult => {
+    const periodLength = dailyHours.length;
+    const trainingDays = dailyHours.filter(hours => hours > 0).length;
+    let success = false;
+    if (trainingDays >= periodLength) {
+        success = true;
+    }
+    const average = dailyHours.reduce((a, b) => a + b, 0) / periodLength;
+
+    let rating : number;
+    let ratingDescription : string;
+
+    if (average < target) {
+        rating = 1;
+        ratingDescription = 'not too bad but could be better';
+    } else if (average === target) {
+        rating = 2;
+        ratingDescription = 'good but you need to put in more effort!';
+    } else {
+        rating = 3;
+        ratingDescription = 'You are doing great!';
+    }
+
+    return {
+        periodLength,
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+   }
+
+const target = Number(process.argv[2]);
+const dailyHours = process.argv.slice(3).map(Number);
+
+if (process.argv.length < 4) {
+    throw new Error('Not enough arguments');
+}
